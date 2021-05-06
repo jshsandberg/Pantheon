84dfa0595c0adffc2780be1f4e0ee72f4c89acf3
jshsandberg <jshsandberg@gmail.com>
2021-05-06 14:01:19 -0700

    test

--- server.js
+++ server.js
@@ -14,14 +14,10 @@ app.use(express.json());
 app.use(cors());
 
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-	});
-}
+// Serve up static assets (usually on heroku)
+app.use(express.static(path.join(__dirname, 'build')));
+
 // Connect to the Mongo DB
 mongoose
 	.connect(process.env.MONGODB_URI || "mongodb://localhost/pantheon", {
